2079a72aa8daefef57fded960878d40b0d75fa15
Heli Eronen <herojonen@gmail.com>
2023-12-27 23:18:38 +0200

    Better styling for HabitView

--- src/components/HabitView.jsx
+++ src/components/HabitView.jsx
@@ -1,19 +1,34 @@
-import { Button, Container } from 'react-bootstrap'
+import { Button, Col, Container, Row } from 'react-bootstrap'
 
 const HabitView = ({ habit, setView }) => {
   return (
-    <Container>
+    <Container className='content-container'>
       <Container className='button-container'>
         <Button onClick={() => setView('home')}>Home</Button>
       </Container>
-      <h1>{habit.name}</h1>
-      <Button>Modify settings</Button>
-      <br></br>
-      <h3>Daily progress</h3>
-      <p>Daily goals here</p>
-      <h2>Other kinds of graphs here</h2>
-      <Button>Add new graph</Button>
-      <Button onClick={() => setView('datapoints')}>See data points</Button>
+      <Container>
+        <Row>
+          <h1 className='habitview-header'>{habit.name}</h1>
+        </Row>
+        <Container className='habitview-button-bar'>
+          <Row>
+            <Col sm='6' style={{ display: 'flex', justifyContent: 'right', paddingRight: '10px' }}>
+              <Button>Modify settings</Button>
+            </Col>
+            <Col sm='3'>
+              <Button onClick={() => setView('datapoints')}>See data points</Button>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+      <Container>
+        <h5 className='habitview-subheader'>Daily progress</h5>
+        <h5 className='habitview-subheader'>Daily goals</h5>
+        <h5 className='habitview-subheader'>Other kinds of graphs here</h5>
+      </Container>
+      <Container className='button-container'>
+        <Button>Add new graph</Button>
+      </Container>
     </Container>
   )
 }
